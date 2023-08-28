3dec9c48fd4751c0c616a5319a02be4e6813bfc0
Subroto Saha <22-92432-1@student.aiub.edu>
2023-08-28 17:46:33 +0600

    sub sidebar seperated

--- src/@fake-db/index.js
+++ src/@fake-db/index.js
@@ -1,7 +1,6 @@
 import mock from './mock'
 import './cards'
 import './table'
-import './auth/jwt'
 import './apps/chat'
 import './pages/faq'
 import './apps/email'
@@ -15,7 +14,5 @@ import './iconify-icons'
 import './app-bar-search'
 import './apps/permissions'
 import './pages/help-center'
-import './server-side-menu/vertical'
-import './server-side-menu/horizontal'
 
 mock.onAny().passThrough()
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,20 +1,58 @@
-import fs from 'fs'
-import * as path from 'path'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
-import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
+ const path = require('path')
+// const webpack = require('webpack')
+// const nodeGlobalsPolyfillPlugin = require('@esbuild-plugins/node-globals-polyfill')
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'bundle.js'
+//     },
+//     resolve: {
+//         alias: {
+//             'src$': path.resolve(__dirname, 'src'),
+//             stream: 'stream-browserify',
+//             crypto: 'crypto-browserify',
+//            "@src": path.resolve(__dirname, 'src'),
+//             '@store': path.resolve(__dirname, 'src/redux'),
+//             '@configs': path.resolve(__dirname, 'src/configs'),
+//             url: 'rollup-plugin-node-polyfills/polyfills/url',
+//             '@styles': path.resolve(__dirname, 'src/@core/scss'),
+//             util: 'rollup-plugin-node-polyfills/polyfills/util',
+//             zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+//             '@utils': path.resolve(__dirname, 'src/utility/Utils'),
+//             '@hooks': path.resolve(__dirname, 'src/utility/hooks'),
+//             '@assets': path.resolve(__dirname, 'src/@core/assets'),
+//             '@layouts': path.resolve(__dirname, 'src/@core/layouts'),
+//             assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+//             buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+//             process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
+//             '@components': path.resolve(__dirname, 'src/@core/components'),
+//         }
+//     },
+// plugins: [
+//     nodeGlobalsPolyfillPlugin({
+//         buffer: true,
+//         process: true
+//     })
+// ]
+// }
+const sassLoader = require('sass-loader')
+
 module.exports = {
-    resolve: {
-        fallback: {
-            crypto: require.resolve("crypto-browserify"),
-            stream: require.resolve("stream-browserify"),
-            fs: false,
-            path: require.resolve("path-browserify"),
-            os: false ,
-            zlib: require.resolve("rollup-plugin-node-polyfills/polyfills/zlib"),
-            url: require.resolve("rollup-plugin-node-polyfills/polyfills/url"),
-            assert: require.resolve("rollup-plugin-node-polyfills/polyfills/assert"),
-            buffer: require.resolve("rollup-plugin-node-polyfills/polyfills/buffer-es6"),
-            process: require.resolve("rollup-plugin-node-polyfills/polyfills/process-es6"),
-        }
+    module: {
+        entry: './index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    sassLoader,
+                ]
+            }
+        ]
     }
 }
